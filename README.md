460d951891485262c1751554d905a09693f1aba7
aviflombaum <avi@flombaum.com>
2015-09-08 14:15:27 -0400

    added solution

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ resources:
 # Ttt 5 Move Rb
 
 *Idea: Give them #display_board so they can see output*
+
+*Problem with move not validating for next unit*
--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
 def move(board, location, current_player = "X")
   board[location.to_i-1] = current_player
 end
+
+puts "Welcome to Tic Tac Toe"
+board = Array.new(9, " ")
+puts "Where would you like to go?"
+input = gets.strip
+
+move(board, input, "X")
+display_board(board)
