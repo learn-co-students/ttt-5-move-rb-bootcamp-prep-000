6ac504a5d61f898f89aab97480176c839c4b96a7
aviflombaum <avi@flombaum.com>
2015-09-09 14:58:35 -0400

    good stub of gets

--- lib/move.rb
+++ lib/move.rb
@@ -6,15 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
 def move(board, location, current_player = "X")
   board[location.to_i-1] = current_player
 end
 
-puts "Welcome to Tic Tac Toe"
-board = Array.new(9, " ")
-puts "Where would you like to go?"
-input = gets.strip
-
-move(board, input, "X")
-display_board(board)
+def user_input
+  gets.strip
+end
