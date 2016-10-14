43a89f440fbb87f425d32e05b8f4e141d8493bac
Cathy Yee <catyyee@hotmail.com>
2016-10-14 14:22:10 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(string = -1 )
   index = (string.to_i) - 1
 end
 
-def move(array, index, value = "X")
-  array[index] = value
+def move(board, index, value = "X")
+  board[index] = value
 end
