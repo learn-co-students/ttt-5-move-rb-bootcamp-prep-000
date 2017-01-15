ef8fa390fb44f20bab165026281f8eecc6b59daa
Megan McLean <mmclean@protonmail.com>
2017-01-15 04:36:47 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 13: Tic Tac Toe Move
+
+# Define method that displays the game board.
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +9,21 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+
+# Define a method, 'input_to_index', that accepts one parameter(s):
+# 'user_input', the string that represents the cell on the game board that the
+# user wants to place their 'X' or 'O'.
+# Convert 'user_input' to an integer and calculate the corresponding index.
+def input_to_index(user_input)
+  index = (user_input.to_i) - 1
+end
+
+
+# Define a method, 'move', that accepts three parameter(s):
+# 'board', the board array,
+# 'index', the index in the board that the user wants to fill with 'X' or 'O',
+# 'char', the user's character (default should be 'X').
+# Update the 'board' array with the user's selection and return it.
+def move(board, index, char = "X")
+  board[index] = char
+end
