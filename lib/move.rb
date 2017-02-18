51ba6fe7e735e20268e24ab7405ef17c5be25e37
Mendy Green <mendygreen@hotmail.com>
2017-02-18 22:17:03 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,16 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(non_index)
+  i = non_index.to_i
+  if i > 0 && i < 10
+    return i - 1
+  end
+  return -1
+end
+
+def move(board, human, player_pos = "X")
+ if board[human] == " "
+   board[human] = player_pos
+ end
+end
