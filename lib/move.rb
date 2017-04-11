1aa68f4dedd824abde3f3b48fbfae480bbc350a5
tao liu <werewaswas@live.com>
2017-04-11 06:41:52 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(array, index, player="X")
+  array[index] = player
+end
+
 # code your input_to_index and move method here!
