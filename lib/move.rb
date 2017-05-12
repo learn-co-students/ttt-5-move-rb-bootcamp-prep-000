bb33f0469a9778f431800376933e8c7ea0fd859c
Renard Burnett <renxburnett@gmail.com>
2017-05-12 17:49:32 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,34 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+
+def display_board(board_arr)
+  puts " #{board_arr[0]} | #{board_arr[1]} | #{board_arr[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board_arr[3]} | #{board_arr[4]} | #{board_arr[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board_arr[6]} | #{board_arr[7]} | #{board_arr[8]} "
+  return board_arr
 end
 
-# code your input_to_index and move method here!
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
+
+def move(arr, index, val = "X")
+  arr[index] = val
+  return arr
+end
+# 
+# ////////
+# puts "Hi! Welcome to the wonderful world of Ruby programming."
+# puts "Please enter your name so that we can greet you more personally:"
+# name = gets.strip
+# greeting(name)
+# ///////
+# 
+# def greeting(name)
+#   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+# end
+# 
+# 
+
