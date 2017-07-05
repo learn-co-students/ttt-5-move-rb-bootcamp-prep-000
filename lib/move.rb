d09b6e7184c1e04825d5403c27308db00daa7055
Joseph Moloughney <jmoloughney25@gmail.com>
2017-07-05 23:10:44 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+  puts "Welcome to Tic Tac Toe"
+  puts "Where would you like to go?"
