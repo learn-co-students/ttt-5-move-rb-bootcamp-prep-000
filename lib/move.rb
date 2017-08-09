a7fc3dc84f7d562850a2b57bf513ca128b9c26eb
William Scripps <wlscripps@gmail.com>
2017-08-09 20:58:46 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,16 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_in)
+  num = (user_in.to_i) - 1
+  return num
+end
+
+def move(array, index, player = "X")
+  array[index] = player
+end
+
+def greeting(name)
+  greet = "Hello #{name}! Let's play Tic Tac Toe!"
+  return greet
+end
