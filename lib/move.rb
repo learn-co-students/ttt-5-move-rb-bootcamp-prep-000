35f054dfcfa948e80db5dbd491350188ffb9ffb6
Jason Slee <Slee@Jasons-iMac.hsd1.co.comcast.net>
2017-08-25 00:12:46 -0600

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +9,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+#Your #move method must take in three arguments, the board array, the index in the board array that the player would like to fill out with an "X" or an "O", and the player's character (either "X" or "O"). The third argument, the player's character, should have a default of "X".
+def move(board, index, player="X")
+  board[index] = player
+end
