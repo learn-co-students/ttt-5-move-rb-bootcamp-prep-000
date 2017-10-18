4d1a10b72f14ad81017e34f93c22820c49407932
Rach <yellatme740@hotmail.com>
2017-10-18 22:03:18 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, position, char="X")
-  board[position] = char
+def move(board, position, marker="X")
+  board[position] = marker
 end
