e86fe9a951165e65b79bb84b54a227a94cb04e4d
Margaretha Juan <tulip_holy@yahoo.com>
2017-11-01 01:26:38 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,24 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board = board)
+  if(board != nil && board.any?)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input_to_index)
+  input_to_index = input_to_index.to_i - 1
+  return input_to_index
+end
+
+def move(board, index, value = "X")
+  if(index > -1 && index < 9)
+    board[index] = value;
+  end
+  return board
+end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
