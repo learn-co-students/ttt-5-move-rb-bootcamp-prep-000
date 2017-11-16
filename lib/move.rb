0a70079d92554df1106ab852c17902ea8a4d3cdd
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-16 20:21:34 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,16 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  if (user_input === "invalide")
+    return -1
+  else
+  input = user_input.to_i
+  input = input - 1
+ end
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+  puts board
+end
