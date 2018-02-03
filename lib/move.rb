5e508fc1444ad53192b729fcf62ea7670aa3a0dd
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-03 00:32:36 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,18 @@
+
+turn_counter = 1
+player_sign_determinant = turn_counter.even?
+
+def player_sign
+  if player_sign_determinant == TRUE
+    player_sign = "0"
+  else
+    player_sign = "X"
+  end
+end
+    
+
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +22,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(move)
+  index = move.to_i - 1
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+
+end
