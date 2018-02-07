da0e8c9bd24a4dfae21cf82fe667c1540d24f825
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 15:39:22 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 puts "Welcome to Tic Tac Toe!"
 puts "Where would you like to go?"
-input = gets.chomp.to_i
+input = gets.to_i
 
 def input_to_index(input)
   board[input + 1] 
