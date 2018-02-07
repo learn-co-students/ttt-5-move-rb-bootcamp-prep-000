99a494d137226373e072699e52bb071108908a11
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 15:32:14 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,8 +1,8 @@
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts "-"*11
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts "-"*11
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
