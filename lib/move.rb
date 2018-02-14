0bbb04daa3c61be3dc6393b8ba606462eef3c461
Dawn Ann <vikingsheepart@yahoo.com>
2018-02-14 21:50:09 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(position_input) #from user input collected and stored by bin
+    position_index = position_input.to_i - 1 #adjust input to match board array index integer value
+    return position_index #want for use board array index positon of token move
+end
+
+def move(board, position_index, token = "X") #moving requires board, position, token info (default X)
+    board[position_index] = "#{token}" #players token: either X or O
+    return board #updated board
+end
