9b39eae7c067584cb850f0a72280f6bb2124d8ed
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-27 05:07:04 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(input)
   return input.to_i - 1
 end
 
-def move(board, index, character='X')
+def move(board, index, character="X")
   board[index] = character
   return board
 end
