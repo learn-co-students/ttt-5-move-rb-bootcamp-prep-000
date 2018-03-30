4d911b5749cb90dd0a82701e0fe59055fa3b887f
John Kalinauskas <jkalinauskas@live.com>
2018-03-30 15:46:04 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,16 @@
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[3]} | #{board[4]}  #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # code your input_to_index and move method here!
+puts "Pick a numner between 1 and 9."
+player_pick = gets.strip
+token = "X"
+player_pick.to_i
+
+
+
