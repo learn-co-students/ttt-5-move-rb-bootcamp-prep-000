05048f9ec1adceb436f66054e47705f085125f71
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-03-31 01:41:21 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(move)
+  move.to_i - 1
+end
+
+def move(board, index, character="X")
+  board[index] = character
+end
+
+def get_variable_from_file(file, variable)
+  
+end
