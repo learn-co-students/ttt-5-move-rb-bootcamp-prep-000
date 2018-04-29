fa815e971d261e5c41d77b046194a443cc41b015
Adrian Davis <chuck.d.kc@gmail.com>
2018-04-29 19:12:23 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,12 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+#convert user input to proper index
 def input_to_index(user_input)
     user_input.to_i - 1
 
 end
 
+#updating board array
 def move(board,index,char="X")
     board[index]=char
 end
