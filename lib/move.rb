0dfab7f7813e1e40e686f7d5e27522c50d90d44c
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-30 14:22:21 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+# player  default top left X
+
+def move(board, index, value="X")
+  board[index] = value
+end
+
+def input_to_index(user_input)
+ user_input = user_input.to_i - 1
+end
