6504760c9359cb650238a30a28d52a5660e28bb9
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-02 22:44:16 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tic Tac Toe CLI: Adding Player Movement to the Game Board
+ in # Tic Tac Toe CLI: Adding Player Movement to the Game Board
 
 ## Objectives
 
--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,17 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
