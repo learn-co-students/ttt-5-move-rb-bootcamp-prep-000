d139a4c9382df34124767f0f5615a68ab573c2f0
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-06 10:38:25 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(i)
+  return (i.to_i - 1)
+end
+
+def move(board, ind, arg = 'X')
+  board[ind] = arg
+end
