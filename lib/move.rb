4105d3b0af9e010a976cb27dc3027ddf5d09cbe4
Brennan B <bdbasinger777@gmail.com>
2018-07-09 21:21:14 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 # code your input_to_index and move method here!
+def input_to_index(idx)
+  idx.to_i - 1
+end
+
+def move(board_arr, idx, player_char="X")
+  board_arr[idx] = player_char
+  board_arr
+end
+
+# board arry 
+# index in the board array that the player would like to fill out 
+# player's character of "X", or "O"
