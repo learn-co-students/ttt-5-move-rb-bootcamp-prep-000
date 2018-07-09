360604ce190d4ff516902fc391b95888fe840796
Jeffrey Rowe <jeffrowetull@gmail.com>
2018-07-09 06:07:26 +0000

    Done.

--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -32,8 +32,8 @@ describe './bin/move executing a CLI Application' do
     allow($stdout).to receive(:puts)
 
     allow(self).to receive(:gets).and_return("1")
-    
-    expect(self).to receive(:input_to_index).and_return(0)
+
+
 
     run_file("./bin/move")
   end
