f8f259c38e6531341d3ff51d6e9232c66562b3c1
Nimer Bashir <nibashir1@gmail.com>
2018-07-30 20:58:27 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,18 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+puts "Welcome to Tic Tac Toe!"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+puts "Where would you like to go?"
+input = gets.strip
+index = input_to_index(input)
+new_board = move(board, index, "X")
+display_board(new_board)
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, token="X")
+  board[index] = token
+  board
+end
