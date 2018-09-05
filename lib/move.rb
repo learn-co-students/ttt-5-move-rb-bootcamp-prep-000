345afbd537fcacfa1ecb1ef4bca751d002538e98
jill zhang <talktojillzhang@gmail.com>
2018-09-05 04:19:53 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,3 @@
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -1,4 +1,5 @@
 require_relative "../lib/move.rb"
+require_relative "spec_helper.rb"
 
 describe './bin/move executing a CLI Application' do
   it 'defines a board variable' do
@@ -6,7 +7,7 @@ describe './bin/move executing a CLI Application' do
     allow(self).to receive(:gets).and_return("1")
     allow(self).to receive(:move)
 
-    board = get_variable_from_file("../bin/move", "board")
+    board = get_variable_from_file("./bin/move", "board")
 
     expect(board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])
   end
