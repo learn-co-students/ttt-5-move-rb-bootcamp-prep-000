8e59e9826536738d349236040c548dff55234046
jill zhang <talktojillzhang@gmail.com>
2018-09-05 03:28:24 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +8,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def move(board,index,mark="X")
+  board[index]=mark
+  return board
+end
+  
+  
+def input_to_index(input)
+  return input.to_i-1
+end
--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -6,7 +6,7 @@ describe './bin/move executing a CLI Application' do
     allow(self).to receive(:gets).and_return("1")
     allow(self).to receive(:move)
 
-    board = get_variable_from_file("./bin/move", "board")
+    board = get_variable_from_file("../bin/move", "board")
 
     expect(board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])
   end
