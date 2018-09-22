2f1925421e3d1941a979455842a2e8838e15262d
ckim31093 <ckim31093@gmail.com>
2018-09-22 17:58:52 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,16 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  index= user_input.to_i - 1
+end
+
+board = [" ", " ", " "]
+  def update_array_at_with(array, index, value)
+    array[index] = value
+  end
+  
+def move(board, index, character = "X")
+  update_array_at_with(board, index, character)
+  board
+end
