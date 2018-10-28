72d71192384c6a910168cd40c0cec578dbd8a3f5
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-10-28 23:57:16 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,20 @@
 def display_board(board)
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  
 end
 
+def input_to_index(number)
+    puts("Please enter a number")
+    number.to_i-1 
+end
+
+def move(board, number, char="X")
+  board[number] = char
+end
+  
 # code your input_to_index and move method here!
