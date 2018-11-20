42f1261c853626979b34abe9119ede9ff3a6a218
Kane Koo <kanekoo@gmail.com>
2018-11-20 04:22:47 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,16 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# code your input_to_index and move method
+  
+def input_to_index (inputvar)
+  index = inputvar.to_i - 1 
+  return index
+end
+
+def move (board, index, value="X")
+  board [index] = value
+  return board
+end
+
+
