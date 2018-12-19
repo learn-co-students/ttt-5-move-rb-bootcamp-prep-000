0108f8c09511a38c27808115fa38d8c541eb0eaa
Joe Bigio <jvbigio@gmail.com>
2018-12-19 01:50:53 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,27 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(string)
+  string.to_i - 1
+end
+# puts "Welcome to Tic Tac Toe!"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# puts "Where would you like to go?"
+# # input = gets.strip
+
+def move(board, index, user_marker = 'X')
+    board[index] = user_marker
+    board
+  end
+  
+#   #MAKE TURN
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   #get the user input
+#   user_input = gets.strip
+#   #input to index
+#   index = input_to_index(user_input)
+#   token = current_player(board)
+# end
+# display_board(board)
