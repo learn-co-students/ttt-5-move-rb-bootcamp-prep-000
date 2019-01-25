902d24fb675054a15e3df01cc2b160b7aa76141e
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-25 19:58:00 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+# The input_to_index method will take the user's input ("1"-"9")
+# and convert it to the index of the board array (0-8).
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
