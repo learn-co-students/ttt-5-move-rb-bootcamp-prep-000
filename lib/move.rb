f47bf9bafb67b21f74b49e058575174309412339
abstract-animation-9914 <vm930@hotmail.com>
2019-02-07 16:26:43 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+   input.to_i - 1
+end 
+
+def move(board, board_index, character = "X")
+  board[board_index] = character
+end 
