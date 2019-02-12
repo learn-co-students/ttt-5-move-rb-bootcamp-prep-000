b604c32f3f36b384181104f88c3c8b112eb01240
Dan-Vy Le <danvyle85@gmail.com>
2019-02-12 00:52:36 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +8,25 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+
+puts "Welcome to Tic Tac Toe!"
+puts "Where would you like to go? (Select from 1-8)"
+
+user_input = STDIN.gets.chomp.to_i
+
+def input_to_index(user_input)
+  converted_input = user_input.to_i - 1
+end
+
+place = input_to_index(user_input) 
+
+def move (board, place, character="X")
+  board[place] = character
+  display_board(board)
+end
+
+move(board, place, "O")
+
+
+
