44788c731506b75dc99e5b8d24246362f0181058
mcumm64 <mcumm64@gmail.com>
2019-03-15 21:46:13 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 # code your input_to_index and move method here!
+
+
+def input_to_index (num) #Want cell 1 means fill board[0]
+  converted_input = (num).to_i - 1
+  return converted_input
+end
+  
+  #converted_input = index for method below?
+  
+def move (array, index, value = "X") #select array to modify, which index of array, and what to put in
+  array[index] = value
+  return display_board(array)
+end
