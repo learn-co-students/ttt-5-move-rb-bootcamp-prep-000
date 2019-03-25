5a492aa3edc0ab4183dd87f24e7cdfb06a791df0
Segundo Bartra <segundobartra@gmail.com>
2019-03-25 10:19:03 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,22 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def init_board(board)
+  initVal = " "
+  for i in 0..8 do
+    board << initVal
+  end
+end
+
+def input_to_index(move)
+  array_location = move.to_i - 1
+  if array_location < 0 || array_location > 8
+    return -1
+  end
+  return array_location
+end
+
+def move(board,index,player = "X")
+  board[index] = player
+  return board
+end
