d9b2ceffdbb72f132b81dedabba09829b2e039d1
Larry Sass-Ainsworth <larry.sassainsworth@gmail.com>
2019-05-20 21:47:58 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(string)
+  string = string.to_i - 1
+end
+
+def move(board_array, index, character="X")
+  board_array[index] = character
+  return board_array
+end 
