c059e706d5b208094b00b8ca3b50a034d5cb5ef4
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-01 14:18:05 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-#board = [" "," "," "," "," "," "," "," "," "]
+
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -16,7 +16,8 @@ def input_to_index(user_input)
   index = input - 1
 end
 
-
-def move(array, index, value)
-  array[index] = value
+board = [" "," "," "," "," "," "," "," "," "]
+def move(board, index, value = "X")
+  board[index] = value
+#  value = "X"
 end
