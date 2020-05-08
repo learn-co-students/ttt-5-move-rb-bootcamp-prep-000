196beef2274e6f23aae62ad3f7ea687951cad3f7
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-08 22:00:59 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+puts "Welcome to Tic Tac Toe!"
+puts "Where would you like to go?"
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +10,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+puts "Enter the number from 1 to 9"
+def input_to_index(user_input)  #get index
+  return user_input.to_i - 1
+end
+
+def move(board, index, symbol = "X")   #get symbol
+  board[index] = symbol
+end
+  
