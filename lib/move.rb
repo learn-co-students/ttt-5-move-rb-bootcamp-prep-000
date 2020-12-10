8900081902879ec9a805f33f49761c3b8c10267f
Nahomie ND <nducasse3@gmail.com>
2020-12-10 05:32:25 +0000

    Defined a method to convert a user's input to an array index. Defined a method that updtes an aarray passed to it. Defined a method with a default value. Used a method in a CLI. Accpet user input via gets in a CLI. Use User input in a method.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+require "pry"
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +7,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(array, index, value="X")
+  array[index] = value
+end
